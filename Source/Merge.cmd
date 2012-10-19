4c7a2cca7766f2f9256ea24658f2d0407b1fbb04
Alexandr Zaozerskiy <alexandr.zaozerskiy@lokad.com>
2012-10-19 16:45:01 +0600

    Removed old sources.

--- Source/Merge.cmd
+++
@@ -1,1 +0,0 @@
-Tool\ilmerge /lib:Library /out:Lokad.CodeDsl.dll MessageContracts/bin/Debug/Lokad.CodeDsl.dll Library/Antlr3.Runtime.dll
--- Source/MessageContracts/IGenerateCode.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.CodeDom.Compiler;
-
-namespace Lokad.CodeDsl
-{
-	public interface IGenerateCode
-	{
-		void Generate(Context context, IndentedTextWriter writer);
-	}
-}
