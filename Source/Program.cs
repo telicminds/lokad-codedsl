cd21506146aacfd6d02fabf569fc6ecfcbb4e094
Alexandr Zaozerskiy <alexandr.zaozerskiy@lokad.com>
2012-11-20 11:14:52 +0600

    Left click on tray icon to display last message.

--- Source/Program.cs
+++ Source/Program.cs
@@ -19,10 +19,14 @@ namespace Lokad.CodeDsl
 {
     class Program
     {
+        public const string FileNamePattern = "*.ddd";
         static readonly Mutex AppLock = new Mutex(true, "2DB34E68-F80D-4ED0-975A-409C2CDAF241");
 
         static readonly ConcurrentDictionary<string, string> States = new ConcurrentDictionary<string, string>();
         public static NotifyIcon TrayIcon;
+        static string _lastTitle;
+        static string _lastMessage;
+        static ToolTipIcon _lastIcon;
 
         static void Main(string[] args)
         {
@@ -41,32 +45,19 @@ namespace Lokad.CodeDsl
                 TrayIcon.Icon = new Icon(iconStream);
 
             TrayIcon.Click += TrayIconClick;
+            TrayIcon.ContextMenu = new ContextMenu(
+                new[] { new MenuItem("Close", (sender, eventArgs) => Close())});
 
             var path = FigureOutLookupPath(args);
             var info = new DirectoryInfo(path);
-            var tipText = string.Format("Using lookup path: {0}\r\nClick icon to exit.", info.FullName);
             TrayIcon.ShowBalloonTip(5000, "Dsl started", tipText, ToolTipIcon.Info);
-
-            var files = info.GetFiles("*.ddd", SearchOption.AllDirectories);
-
-            foreach (var fileInfo in files)
-            {
-                var text = File.ReadAllText(fileInfo.FullName);
-                Changed(fileInfo.FullName, text);
-                try
-                {
-                    Rebuild(text, fileInfo.FullName);
-                }
-                catch (Exception ex)
-                {
-                    TrayIcon.ShowBalloonTip(10000, "Parse error - " + fileInfo.Name, ex.Message, ToolTipIcon.Error);
-                }
-            }
+            
+            var files = info.GetFiles(FileNamePattern, SearchOption.AllDirectories);
 
             var notifiers = files
                 .Select(f => f.DirectoryName)
                 .Distinct()
-                .Select(d => new FileSystemWatcher(d, "*.ddd"))
+                .Select(d => new FileSystemWatcher(d, FileNamePattern))
                 .ToArray();
 
             foreach (var notifier in notifiers)
@@ -75,9 +66,51 @@ namespace Lokad.CodeDsl
                 notifier.EnableRaisingEvents = true;
             }
 
+            var message = string.Format(
+                "Lookup path: {0}{1}", 
+                info.FullName, Environment.NewLine);
+
+            if (files.Any())
+            {
+                message += String.Join("\r\n", files.Select(x => x.Name));
+            }
+            else
+            {
+                message += "Found no files () to watch";
+            }
+
+            message += "\r\n\r\nClick icon to see last message.";
+
+
+            ShowBalloonTip("Dsl started", message, ToolTipIcon.Info);
+
+            foreach (var fileInfo in files)
+            {
+                var text = File.ReadAllText(fileInfo.FullName);
+                Console.WriteLine("  Watch: {0}", fileInfo.Name);
+                Changed(fileInfo.FullName, text);
+                try
+                {
+                    Rebuild(text, fileInfo.FullName);
+                }
+                catch (Exception ex)
+                {
+                    ShowBalloonTip("Parse error - " + fileInfo.Name, ex.Message, ToolTipIcon.Error);
+                }
+            }
+
             AppDomain.CurrentDomain.ProcessExit += CurrentDomainProcessExit;
             Application.ThreadExit += ApplicationThreadExit;
             Application.Run(new Empty());
+        }
+
+        private static void ShowBalloonTip(string title, string message, ToolTipIcon toolTipIcon)
+        {
+            _lastTitle = title;
+            _lastMessage = message;
+            _lastIcon = toolTipIcon;
+
+            TrayIcon.ShowBalloonTip(10000, _lastTitle, _lastMessage, _lastIcon);
         }
 
         static void ApplicationThreadExit(object sender, EventArgs e)
@@ -92,16 +125,17 @@ namespace Lokad.CodeDsl
 
         static void TrayIconClick(object sender, EventArgs e)
         {
-            Close();
-            Application.Exit();
+            ShowBalloonTip(_lastTitle, _lastMessage, _lastIcon);
         }
 
-        private static void Close()
+        static void Close()
         {
             if (TrayIcon != null)
             {
                 TrayIcon.Dispose();
             }
+
+            Application.Exit();
         }
 
         static string FigureOutLookupPath(string[] args)
@@ -134,18 +168,18 @@ namespace Lokad.CodeDsl
                     return;
 
 
-                var message = string.Format("File {1}-{0}", args.Name, args.ChangeType);
+                var message = string.Format("Changed: {1}-{0}", args.Name, args.ChangeType);
                 Console.WriteLine(message);
                 Rebuild(text, args.FullPath);
 
-                TrayIcon.ShowBalloonTip(3000, args.Name, "File rebuilded", ToolTipIcon.Info);
+                ShowBalloonTip(args.Name, "File rebuilded", ToolTipIcon.Info);
                 SystemSounds.Beep.Play();
             }
             catch (IOException) {}
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
-                TrayIcon.ShowBalloonTip(10000, "Error", ex.Message, ToolTipIcon.Error);
+                ShowBalloonTip("Error - " + args.Name, ex.Message, ToolTipIcon.Error);
 
                 SystemSounds.Exclamation.Play();
             }
@@ -174,15 +208,13 @@ namespace Lokad.CodeDsl
                     GenerateInterfaceForEntityWithModifiers = "?",
                     TemplateForInterfaceName = "public interface I{0}Aggregate",
                     TemplateForInterfaceMember = "void When({0} c);",
-                    ClassNameTemplate = @"
-    
-
-[DataContract(Namespace = {1})]
+                    ClassNameTemplate = @"[DataContract(Namespace = {1})]
 public partial class {0}",
                     MemberTemplate = "[DataMember(Order = {0})] public {1} {2} {{ get; private set; }}",
                     
                 };
 
+  
             File.WriteAllText(Path.ChangeExtension(fullPath, "cs"), GeneratorUtil.Build(dsl, generator));
         }
     }
